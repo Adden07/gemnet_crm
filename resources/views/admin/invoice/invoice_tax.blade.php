6f89dd36b7f513fd5590e309b38410ff1ec64c95
Adden07 <anasrajput3@gmail.com>
2023-05-22 13:48:02 +0500

    changes

--- resources/views/admin/invoice/invoice_tax.blade.php
+++ resources/views/admin/invoice/invoice_tax.blade.php
@@ -33,7 +33,7 @@
                         <select class="form-control" name="date" id="date" required>
                             <option value="">Select month</option>
                             @foreach($months AS $month)
-                                <option value="{{ $month->created_at }}">{{ date('M-y', strtotime($month->created_at)) }}</option>
+                                <option value="{{ $month->created_at }}">{{ date('F-y', strtotime($month->created_at)) }}</option>
                             @endforeach
                         </select>
                     </div>
