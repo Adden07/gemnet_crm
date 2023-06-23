16ff1ab0fe135231eb82e0b48a89608b6720d6cc
Adden07 <anasrajput3@gmail.com>
2023-06-23 16:07:26 +0500

    changes

--- app/Helpers/CommonHelpers.php
+++ app/Helpers/CommonHelpers.php
@@ -310,7 +310,7 @@ class CommonHelpers
             $sms_status = $sms->status;
         }
         // dd($setting->is_sms);
-        if($setting->is_sms == 1 && ($sms_status == 1 || $sms_type == 'manual')){
+        if($setting->is_sms == 1){
             dd('dones');
             $params = [
                 'id'    => config('sms.sms_api_id'),
